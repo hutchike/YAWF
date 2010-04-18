2ca13dad7a031837a3d6c354455fbcbecbaab2ea
Your Name <kevin.hutchinson@legendum.com>
2010-04-18 02:47:14 -0700

    Added a "queue" directory to hold temporary queue data for daemons

---
+++ yawf/controllers/App.php
@@ -0,0 +1,24 @@
+<?
+// Copyright (c) 2009 Guanoo, Inc.
+// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+
+class App_controller extends Controller
+{
+    // Replace this by writing your own class in "myapp/app/controllers/App.php"
+
+    function before()
+    {
+        $this->render['title'] = 'Welcome to YAWF';
+    }
+}
+
+// End of App.php
