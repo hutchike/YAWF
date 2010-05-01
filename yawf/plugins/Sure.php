b123a1bfb445737e29f3cc16b6f1a40d244ce382
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-05-01 10:58:06 -0500

    Began adding documentation to the YAWF code

--- yawf/plugins/Sure.php
+++ yawf/plugins/Sure.php
@@ -1,15 +1,75 @@
 <?php
 /**
-   * Sure -- Simple User-defined Rule Engine (SURE)
-   * @version 0.2
-   * @author Kevin Hutchinson <kevin@guanoo.org>
-   * @link http://github.com/hutchike/YAWF
-   * @copyright Copyright 2010 Kevin Hutchinson
-   * @license http://www.opensource.org/licenses/mit-license.php MIT License
-   * @package Sure
-   */
+ * Sure -- Simple User-defined Rule Engine (SURE)
+ *
+ * "Sure" is a simple way to embed an expert system in your PHP web application.
+ * Here's a simple example:
+ *
+ * $sure = new Sure();
+ * $sure->with('rules.sure')->given('facts.sure')->infer();
+ * 
+ * ...where "rules.sure" and "facts.sure" are files written like this:
+ *
+ * rules.sure:
+ * <code>
+ *   rule: Relax on the weekend
+ *   when: $today == 'Sat' or $today == 'Sun'
+ *   then: print "I'm relaxing today, thanks"
+ * </code>
+ *
+ * facts.sure:
+ * <code>
+ *   $today = date('D')
+ * </code>
+ *
+ * The syntax for the rules and the facts is regular PHP code, except that you
+ * may write "or" instead of "||" for readability and the rules should include
+ * "rule:", "when:" and "then:" prefix labels at the start of lines to define
+ * the rules. (You can replace "when:" with "if:" if you prefer).
+ *
+ * If you want a rule to meet many conditions, you can list them on lines like
+ * this:
+ *
+ * <code>
+ * rule: Pay rent on 1st of the month
+ * when:
+ *   $day_of_month == 1
+ *   $rent_unpaid
+ *   $cash_in_pocket >= 4800
+ * then:
+ *   $bank->pay_the_rent()
+ * </code>
+ *
+ * In this example, we've used a PHP object $bank, which may be passed into the
+ * Sure object constructor like this:
+ *
+ * <code>
+ * $sure = new Sure(array('bank' => $bank_object));
+ * $sure->with('rules.sure')->given('facts.sure')->infer();
+ * </code>
+ *
+ * This way, the Sure rules engine can be used to orchestrate your other PHP
+ * code according to flexible rules. Finally, the filenames "rules.sure" and
+ * "facts.sure" may be replaced with string values if you prefer to pass the
+ * rules and facts directly, for example if you store them in a database.
+ *
+ * @version 0.2
+ * @author Kevin Hutchinson <kevin@guanoo.org>
+ * @link http://github.com/hutchike/YAWF
+ * @copyright Copyright 2010 Kevin Hutchinson
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @package Sure
+ */
+
+/**
+ * A class to provide a Simpe User-defined Rule Engine (SURE)
+ * @package Sure
+ */
 class Sure
 {
+    /**
+     * Limit the maximum number of inference iterations through the rules
+     */
     const MAX_INFERENCE_LOOPS = 100;
 
     private $memory;
@@ -17,6 +77,9 @@ class Sure
     private $parsed_rules;
     private $parsed_facts;
 
+    /**
+     * Create a new Sure object to infer from rules and facts
+     */
     public function __construct()
     {
         $this->memory = NULL;
@@ -96,6 +159,10 @@ class Sure
     }
 }
 
+/**
+ * A class to parse rules and facts as PHP code
+ * @package Sure
+ */
 class SureParser
 {
     const CONDITIONS = 1;
@@ -171,6 +238,11 @@ class SureParser
     }
 }
 
+/**
+ * A rule has a name, a list of conditions and a list of actions to take
+ * when all the conditions are met.
+ * @package Sure
+ */
 class SureRule
 {
     private $name;
@@ -250,6 +322,10 @@ class SureRule
     }
 }
 
+/**
+ * A fact may be remembered into a memory variable (see SureMemory)
+ * @package Sure
+ */
 class SureFact
 {
     private $fact;
@@ -266,6 +342,10 @@ class SureFact
     }
 }
 
+/**
+ * A class to remember facts in a simple object hierarchy
+ * @package Sure
+ */
 class SureMemory
 {
     function __construct($data = NULL)
