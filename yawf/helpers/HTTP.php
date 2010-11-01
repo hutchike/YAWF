8b55b9b97368479afb803f2c4e22ae6b6092d966
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-11-01 05:31:28 -0600

    Added a very bare HTTP helper

---
+++ yawf/helpers/HTTP.php
@@ -0,0 +1,19 @@
+<?
+// Copyright (c) 2009 Guanoo, Inc.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+
+class HTTP extends YAWF
+{
+    const MOVED_PERMANENTLY = 301;
+}
+
+// End of HTTP.php
