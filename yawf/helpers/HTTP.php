1960ac0127051a8537a35f0c4772b63762dd2419
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-11-01 05:40:01 -0600

    Added some rather useful HTTP status codes

--- yawf/helpers/HTTP.php
+++ yawf/helpers/HTTP.php
@@ -13,7 +13,26 @@
 
 class HTTP extends YAWF
 {
+    // 300 status codes
+
     const MOVED_PERMANENTLY = 301;
+    const FOUND = 302;
+    const SEE_OTHER = 303;
+    // Add more here...
+
+    // 400 status codes
+
+    const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
+    const METHOD_NOT_ALLOWED = 405;
+    // Add more here...
+
+    // 500 status codes
+
+    const NOT_IMPLEMENTED = 501;
+    const BAD_GATEWAY = 502;
+    const SERVICE_UNAVAILABLE = 503;
+    // Add more here...
 }
 
 // End of HTTP.php
