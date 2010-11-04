a8251b300e3d80aa68d2ea867f6749b0ada12949
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-11-03 23:55:04 -0600

    removed "www"

--- app/views/en/project/faq.php
+++ app/views/en/project/faq.php
@@ -2,7 +2,7 @@
 YAWF has been tested with PHP versions 5.1, 5.2 and 5.3.
 
 <h4>But I've only got PHP4?</h4>
-Sorry, you'll have to upgrade or find <a href="http://www.cakephp.org/">another good PHP framework</a> instead.
+Sorry, you'll have to upgrade or find <a href="http://cakephp.org/">another good PHP framework</a> instead.
 
 <h4>Why did you make yet another web framework?</h4>
 The existing frameworks are good, but they seem big and heavy. YAWF is small, light and fast. Of course, the cost for this is the range of features, but YAWF does include many "standard" features:
@@ -15,7 +15,7 @@ The existing frameworks are good, but they seem big and heavy. YAWF is small, li
 </ul>
 
 <h4>OK, so is it used in the real world anywhere?</h4>
-Yes, it runs <a href="http://zicatela.net/">zicatela.net</a> and <a href="http://www.plumline.org/">www.plumline.org</a>.
+Yes, it runs <a href="http://zicatela.net/">zicatela.net</a> and <a href="http://plumline.org/">plumline.org</a>.
 
 <h4>What are your plans for YAWF?</h4>
 The number one priority right now is to improve the documentation so that more people can start to use YAWF and enjoy its features.
