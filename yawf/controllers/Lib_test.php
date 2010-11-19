ad957d40cb45a0fe0dc3a3de4a2f55c89133969e
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-11-19 05:38:58 -0600

    Lib_test.php

--- yawf/controllers/Lib_test.php
+++ yawf/controllers/Lib_test.php
@@ -80,7 +80,7 @@ class Lib_test_controller extends Controller
                       $this->lang === DEFAULT_LANGUAGE, $this->lang);
         $this->should('have a seesion',
                       is_array($_SESSION), $_SESSION);
-        $this->should('have a flash object', $this->flash instanceof Request_flash, $this->flash);
+        $this->should('have a flash object', $this->flash instanceof Controller_flash, $this->flash);
         $this->should('have a cookie object', $this->cookie instanceof Request_cookie, $this->cookie);
         $this->should('have a server object', $this->server instanceof Request_server, $this->server);
         $this->should('have a session object', $this->session instanceof Request_session, $this->session);
