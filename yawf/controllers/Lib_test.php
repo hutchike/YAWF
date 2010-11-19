883d7e0742b6118f892c1b7a18484e8c090b4338
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-11-19 05:44:24 -0600

    hmmm

--- yawf/controllers/Lib_test.php
+++ yawf/controllers/Lib_test.php
@@ -246,7 +246,7 @@ class Lib_test_controller extends Controller
         $this->flash->now = self::TEST_FLASH;
         $this->should("have a flash notice now", $this->flash->notice == self::TEST_FLASH);
         $this->flash->warning = $this->flash->error = self::TEST_FLASH;
-        $this->flash = $this->new_flash_object(); // to simulate a redirect call
+        $this->flash = new Request_flash(); // simulate a new page
         $this->should("have a flash warning set", $this->flash->warning == self::TEST_FLASH);
         $this->should("have a flash error set", $this->flash->error == self::TEST_FLASH);
     }
