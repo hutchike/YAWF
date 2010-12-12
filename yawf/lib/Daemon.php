14f01c4dfe675d95a08d2f11af504dedc89f7a2a
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2010-12-11 22:40:21 -0600

    more comments

--- yawf/lib/Daemon.php
+++ yawf/lib/Daemon.php
@@ -15,12 +15,24 @@ error_reporting(E_ALL | E_STRICT);
 
 require_once('Command.php');
 
+/**
+ * The Daemon class is a type of command that is designed to run
+ * in the background without any duplicate processes. It achieves
+ * this by keeping note of its PID in a pidfile in the "/tmp/pids"
+ * directory. A Daemon object can't be created if another daemon
+ * is running simultaneously.
+ *
+ * @author Kevin Hutchinson <kevin@guanoo.com>
+ */
 class Daemon extends Command
 {
     private $pid, $pid_file;
 
-    // Quit if we're already running, or start by creating a new PID file
-
+    /**
+     * Quit if we're already running, or start by creating a new PID file
+     *
+     * @param String $dir an optional starting directory to change from
+     */
     public function __construct($dir = NULL)
     {
         parent::__construct($dir);
@@ -28,24 +40,31 @@ class Daemon extends Command
         $this->write_pid_to_file();
     }
 
-    // Delete our PID file and finish
-
+    /**
+     * Delete our PID file and finish
+     */
     public function __destruct()
     {
         $this->delete_pid_file();
         parent::__destruct();
     }
 
-    // Return the PID of our process
-
+    /**
+     * Return the PID of our process
+     *
+     * @return Integer the PID of this process
+     */
     public function pid()
     {
         if ($this->pid) return $this->pid;
         return ($this->pid = getmypid());
     }
 
-    // Return the name of our process's PID file
-
+    /**
+     * Return the path to our process's PID file
+     *
+     * @return String the path to this process's PID file
+     */
     public function pid_file()
     {
         if (!$this->pid_file)
@@ -57,8 +76,11 @@ class Daemon extends Command
         return $this->pid_file;
     }
 
-    // Return whether our process is already running
-
+    /**
+     * Return whether our process is already running
+     *
+     * @return Boolean whether an identical daemon process is already running
+     */
     protected function is_running()
     {
         if ($pid = $this->read_pid_from_file())
@@ -74,25 +96,30 @@ class Daemon extends Command
         }
     }
 
-    // Read a PID from our process's PID file
-
+    /**
+     * Read a PID from our process's PID file
+     *
+     * @return String the PID number in a pidfile for this process
+     */
     protected function read_pid_from_file()
     {
 
         return file_exists($this->pid_file()) ?
                trim(file_get_contents($this->pid_file())) :
-               null;
+               NULL;
     }
 
-    // Write our PID to a PID file
-
+    /**
+     * Write our PID to a PID file
+     */
     protected function write_pid_to_file()
     {
         file_put_contents($this->pid_file(), $this->pid());
     }
 
-    // Delete a PID file if the PID matches ours
-
+    /**
+     * Delete a PID file if the PID matches ours
+     */
     protected function delete_pid_file()
     {
         if ($pid = $this->read_pid_from_file());
@@ -101,8 +128,11 @@ class Daemon extends Command
         }
     }
 
-    // Return the default start directory
-
+    /**
+     * Return the default start directory
+     *
+     * @return String the path to this daemon command
+     */
     protected function start_directory()
     {
         return dirname($this->path); // Daemons start from their own path
--- yawf/lib/YAWF.php
+++ yawf/lib/YAWF.php
@@ -11,8 +11,6 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 
-// Classes should extend YAWF for hooks
-
 /**
  * The YAWF object responds to web requests by creating an "App"
  * object, a "Controller" object, then rendering a web response.
