b2b8b541d522c426002764ddbc77097a8af06f0a
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-01-03 12:08:25 +0000

    more comments

--- app/views/en/project/news.php
+++ app/views/en/project/news.php
@@ -3,7 +3,7 @@ YAWF now supports remote data services over REST. This means you can wrap a
 model object in a "Remote" object like this:
 <?= highlight('
 Remote::set_default("server", "http://some.website.com");
-$u = new User(array("name" => "Mr Smith"));
+$u = new User(array("name" => "Mr Smith")); // "User" class extends "Model"
 $u = new Remote($u); // wrap in remote object
 $u->save(); // save to the remote server
 print "id is " . $u->id; // get the user\'s ID
