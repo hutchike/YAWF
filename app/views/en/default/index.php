085b7bfadddc06a3eb56dcf299e77e2b113c727d
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-01-03 10:48:56 +0000

    Added an index page

--- app/views/en/default/index.php
+++ app/views/en/default/index.php
@@ -1,1 +1,35 @@
-Welcome to <acronym title="Yet Another Web Framework">YAWF</acronym>, a simple lightweight <acronym title="Model View Controller">MVC</acronym> framework written in the popular <a href="http://www.php.net/">PHP language</a>.
+<p>
+    Welcome to <acronym title="Yet Another Web Framework">YAWF</acronym>, a simple lightweight <acronym title="Model View Controller">MVC</acronym> framework written in <a href="http://www.php.net/">PHP 5</a>.
+</p>
+
+<p>
+    YAWF contains only about 5k lines of code and delivers these top 10 features:
+    <ol>
+        <li>Standard <acronym title="Model View Controller">MVC</acronym> architecture</li>
+        <li>Integrated testing (unit, integration, UI)</li>
+        <li>Support for multiple page layouts and various content types</li>
+        <li>Extensibility via "dependency injection" and "drop-in" class replacements</li>
+        <li>Models that support "has_a", "has_many" and "belongs_to" relationships</li>
+        <li>Models that support remote data conections via REST web services</li>
+        <li>Various database connectors (currently supports MySQL and SQLite)</li>
+        <li>Simple REST web services for models and other web services</li>
+        <li>Integrated PHP shell ("<acronym title="Yet Another SHell">yash</acronym>") for scripting and testing</li>
+        <li>Full detailed logging and error reporting by email</li>
+    </ol>
+</p>
+
+<p>
+    If you'd like to know more, please email <a href="mailto:kevin@guanoo.com">kevin@guanoo.com</a> for more information.
+</p>
+
+<p>
+    If you're looking for a PHP framework that's been around for longer,<br/>
+    and has a much larger following of dedicated coders, here are some:
+    <ul>
+        <li><a href="http://www.akelos.org/">Akelos</a></li>
+        <li><a href="http://cakephp.org/">CakePHP</a></li>
+        <li><a href="http://codeigniter.com/">CodeIgniter</a> (recommended)</li>
+        <li><a href="http://www.symfony-project.org/">Symfony</a></li>
+        <li><a href="http://www.zend.com/en/">Zend</a></li>
+    </ul>
+</p>
