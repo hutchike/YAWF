78332b8f8359105cbba8e6d832e95e4911db0958
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-01-06 15:19:12 +0000

    Added some handy links

--- app/views/en/default/index.php
+++ app/views/en/default/index.php
@@ -10,11 +10,11 @@
         <li>Support for multiple page layouts and various content types</li>
         <li>Extensibility via "dependency injection" and "drop-in" class replacements</li>
         <li>Models that support "has_a", "has_many" and "belongs_to" relationships</li>
-        <li>Models that support remote data conections via REST web services</li>
-        <li>Various database connectors (currently supports MySQL and SQLite)</li>
+        <li>Models that support remote data conections via <a href="http://en.wikipedia.org/wiki/Representational_State_Transfer">REST</a> web services</li>
+        <li>Various database connectors (currently supports <a href="http://www.mysql.com/">MySQL</a> and <a href="www.sqlite.org">SQLite</a>)</li>
         <li>Simple REST web services for models and other web services</li>
         <li>Integrated PHP shell ("<acronym title="Yet Another SHell">yash</acronym>") for scripting and testing</li>
-        <li>Full detailed logging and error reporting by email</li>
+        <li>Detailed logging and error reporting by email</li>
     </ol>
 </p>
 
@@ -26,12 +26,12 @@
 
 <p>
     Or if you're looking for a PHP framework that's been around for longer,<br/>
-    and has a much larger following of dedicated coders, here are some:
+    and has a larger following of dedicated coders, here are some suggestions:
     <ul>
         <li><a href="http://www.akelos.org/">Akelos</a></li>
         <li><a href="http://cakephp.org/">CakePHP</a></li>
         <li><a href="http://codeigniter.com/">CodeIgniter</a> (recommended)</li>
-        <li><a href="http://kohanaframework.org/">Kohana</a> (recommended)</li>
+        <li><a href="http://kohanaframework.org/">Kohana</a> (recommended for <acronym title="Hierarchical Model View Controller">HMVC</acronym>)</li>
         <li><a href="http://www.symfony-project.org/">Symfony</a></li>
         <li><a href="http://www.zend.com/en/">Zend</a></li>
     </ul>
