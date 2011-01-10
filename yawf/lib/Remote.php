590da966aea0fa945ca215eaa7c81d82b95d0246
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-01-10 21:42:57 +0000

    fixed a little Remote bug

--- yawf/lib/Remote.php
+++ yawf/lib/Remote.php
@@ -221,6 +221,7 @@ class Remote extends Relating_model implements Modelled, Persisted, Validated
         $params = array();
         if ($where = array_key($conditions, 'where')) $params['where'] = $where;
         else $params['where'] = $this->where_clause();
+        if (!$params['where']) $params['where'] = '1=1'; // coz can't be blank
         $params['join'] = array_key($conditions, 'join');
         $params['order'] = $this->get_order();
         $params['limit'] = $this->get_limit();
