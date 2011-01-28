75b80d05c4d7771e0d4f6250b7245ddca552d18e
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-01-29 01:25:56 +0800

    tests pass

--- app/views/en/default/index.php
+++ app/views/en/default/index.php
@@ -4,19 +4,19 @@
 
 <p>
     YAWF contains only about 5k lines of code and delivers these top 10 features:
-    <ol>
-        <li>Standard <acronym title="Model View Controller">MVC</acronym> architecture</li>
-        <li>Integrated testing (unit, integration, UI)</li>
-        <li>Support for multiple page layouts and various content types</li>
-        <li>Extensibility via "dependency injection" and "drop-in" class replacements</li>
-        <li>Models that support "has_a", "has_many" and "belongs_to" relationships</li>
-        <li>Models that support remote data conections via <a href="http://en.wikipedia.org/wiki/Representational_State_Transfer">REST</a> web services</li>
-        <li>Various database connectors (currently supports <a href="http://www.mysql.com/">MySQL</a> and <a href="www.sqlite.org">SQLite</a>)</li>
-        <li>Simple REST web services for models and other web services</li>
-        <li>Integrated PHP shell ("<acronym title="Yet Another SHell">yash</acronym>") for scripting and testing</li>
-        <li>Detailed logging and error reporting by email</li>
-    </ol>
 </p>
+<ol>
+    <li>Standard <acronym title="Model View Controller">MVC</acronym> architecture</li>
+    <li>Integrated testing (unit, integration, UI)</li>
+    <li>Support for multiple page layouts and various content types</li>
+    <li>Extensibility via "dependency injection" and "drop-in" class replacements</li>
+    <li>Models that support "has_a", "has_many" and "belongs_to" relationships</li>
+    <li>Models that support remote data conections via <a href="http://en.wikipedia.org/wiki/Representational_State_Transfer">REST</a> web services</li>
+    <li>Various database connectors (currently supports <a href="http://www.mysql.com/">MySQL</a> and <a href="www.sqlite.org">SQLite</a>)</li>
+    <li>Simple REST web services for models and other web services</li>
+    <li>Integrated PHP shell ("<acronym title="Yet Another SHell">yash</acronym>") for scripting and testing</li>
+    <li>Detailed logging and error reporting by email</li>
+</ol>
 
 <p>
     If you'd like to know more, please email <a href="mailto:kevin@guanoo.com">kevin@guanoo.com</a> for more information.
@@ -27,12 +27,12 @@
 <p>
     Or if you're looking for a PHP framework that's been around for longer,<br/>
     and has a larger following of dedicated coders, here are some suggestions:
-    <ul>
-        <li><a href="http://www.akelos.org/">Akelos</a></li>
-        <li><a href="http://cakephp.org/">CakePHP</a></li>
-        <li><a href="http://codeigniter.com/">CodeIgniter</a> (recommended)</li>
-        <li><a href="http://kohanaframework.org/">Kohana</a> (recommended for <acronym title="Hierarchical Model View Controller">HMVC</acronym>)</li>
-        <li><a href="http://www.symfony-project.org/">Symfony</a></li>
-        <li><a href="http://www.zend.com/en/">Zend</a></li>
-    </ul>
 </p>
+<ul>
+    <li><a href="http://www.akelos.org/">Akelos</a></li>
+    <li><a href="http://cakephp.org/">CakePHP</a></li>
+    <li><a href="http://codeigniter.com/">CodeIgniter</a> (recommended)</li>
+    <li><a href="http://kohanaframework.org/">Kohana</a> (recommended for <acronym title="Hierarchical Model View Controller">HMVC</acronym>)</li>
+    <li><a href="http://www.symfony-project.org/">Symfony</a></li>
+    <li><a href="http://www.zend.com/en/">Zend</a></li>
+</ul>
