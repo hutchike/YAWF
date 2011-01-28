dcf3b7e45418bfb5ef9eaedfa2ed5e96a470d16f
Kevin Hutchinson <kevin.hutchinson@legendum.com>
2011-01-28 16:15:12 +0800

    sweet!

--- yawf/lib/App_command.php
+++ yawf/lib/App_command.php
@@ -42,6 +42,8 @@ class App_command extends Command
             $this->status();
         elseif ($this->opts->test)
             $this->test();
+        elseif ($this->opts->data)
+            $this->data();
         else
             $this->usage();
     }
@@ -188,6 +190,75 @@ class App_command extends Command
         if ($left == $right) return 0;
         return $left < $right ? -1 : 1;
     }
+
+    /**
+     * (Re)create databases ("live" and "test") using the "mysql" command
+     */
+    private function data()
+    {
+        // Load the app config files
+
+        try {
+            $config = Config::load(hostname());
+            Config::define_constants(array_key($config, 'database', array()),
+                                     array('prefix' => 'DB_'));
+        } catch (Exception $e) {
+            // It's ok when there's no host config
+        }
+        $config = Config::load('app');
+        $database = array_key($config, 'database', array());
+        Config::define_constants($database, array('prefix' => 'DB_'));
+
+        // Configure the database
+
+        $app_name = $this->name;
+        $username = defined('DB_USERNAME') ? DB_USERNAME : 'root';
+        $password = defined('DB_PASSWORD') ? DB_PASSWORD : '';
+        $database_live = defined('DB_DATABASE_LIVE') ? DB_DATABASE_LIVE
+                                                   : "${app_name}_live";
+        $database_test = defined('DB_DATABASE_TEST') ? DB_DATABASE_TEST
+                                                   : "${app_name}_test";
+        $schema_file = defined('DB_SCHEMA') ? DB_SCHEMA : "$app_name.sql";
+        $migrate_file = defined('DB_MIGRATE') ? DB_MIGRATE : 'migrate.sql';
+        $database_live = first($this->opts->database, $database_live);
+        $database_test = first($this->opts->database, $database_test);
+        $schema_file = "app/data/" . first($this->opts->schema, $schema_file);
+        if (!is_file($schema_file)) $this->usage($usage, "No schema file found here: $schema_file");
+        $migrate_file = "app/data/" . first($this->opts->migrate, $migrate_file);
+
+        // Check we're using MySQL
+
+        if (preg_match('/mysql/i', DB_CONNECTOR))
+        {
+            // First apply any migrations
+
+            $login = "-u$username" . ($password ? " -p$password" : '');
+            if (file_exists($migrate_file)) system("yawf_cat $migrate_file | mysql $login $database_live");
+
+            // ...then dump existing data
+
+            $dump_file = "/tmp/$database_live.dump";
+            system("mysqldump --no-create-info --complete-insert $login $database_live > $dump_file");
+
+            // ...then recreate db tables
+
+            system("yawf_cat $schema_file | mysql $login $database_live");
+            system("mysql $login $database_live <$dump_file");
+            if ($database_test != $database_live) {
+                system("yawf_cat $schema_file | mysql $login $database_test");
+            }
+
+            // ...finally remove the data
+
+            if ($this->opts->secure) unlink($dump_file);
+        }
+        else
+        {
+            $connector = DB_CONNECTOR;
+            print "Sorry but database connector $connector isn't supported yet";
+        }
+    }
+
 }
 
 // End of App_command.php
